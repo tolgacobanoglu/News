dcbbcb70f29aa861dd4e40c96cd6a3aa92c3c26c
tolgacobanoglu <1tolgacobanoglu@gmail.com>
2022-08-10 17:43:21 +0300

    initial commit

---
+++ app/src/main/java/com/tolgacobanoglu/newsapp/util/Constant.kt
@@ -0,0 +1,8 @@
+package com.tolgacobanoglu.newsapp.util
+
+object Constant
+{
+    //News API
+    const val API_KEY = "f61561e70bca4787b31fb858b01a5530"
+    const val BASE_URL = "https://newsapi.org"
+}
